cc277cc010e35b12a24a88b01fdec590f5e025f9
memakingbeats <40284030+memakingbeats@users.noreply.github.com>
2024-01-29 15:19:56 -0300

    commit site

---
+++ languages/language-en.js
@@ -0,0 +1,76 @@
+ // language-en.js
+            const translationsEn = {
+                home: "HOME",
+                lathe: "LATHES",
+                styli: "STYLI",
+                blanks: "BLANKS",
+                records: "RECORDS",
+                contact: "CONTACT",
+                contact2: "CONTACT",
+                latheContact:"Contact Us",
+                latheContact2:"Contact Us",
+                description:"South.in Cuts is a supplier of raw materials for Recording lathes. We provide cutting machines, needles, and blank discs. Additionally, we offer disc engraving services with your personalized music. Get in touch with us to acquire high-quality materials and customized solutions for your machining and recording needs.",
+
+                contacttext:
+                    "Contact us, and we will be happy to address all your inquiries about our equipment. Our rotary mixers and recording lathes are available, ready to meet your needs. Feel free to reach out to us for detailed information on how our products can enhance the quality and efficiency of your work. We look forward to assisting you in finding the perfect solution for your projects.",
+
+                SVR84: "The vinyl recording machine SVR-84 - Record Lathe is an impressive evolution of vinyl disc cutting technology.",
+
+                subtitle: "A revolution in cutting records.",
+
+                paragraf:
+                    "Equipped with state-of-the-art features, this machine offers music professionals and recording studios an efficient and highly precise way to create customized vinyl records. The SVR-84 is equipped with advanced features such as CNC precision control, allowing for extremely accurate adjustment of groove depth and width, resulting in exceptional sound quality. Furthermore, its real-time monitoring system and advanced automation ensure the consistency and quality of each recording. Compatible with standard and custom vinyl formats, the SVR-84 meets the demands of musicians, record labels, and audiophiles looking to create exceptional and unique vinyl records in a high-tech production environment. This vinyl recording machine represents the perfect blend of vinyl tradition and modern precision engineering.",
+
+                botao: "Contact Us",
+
+                SVR82: "The vinyl recording machine SVR-82 - Record Lathe is an exceptional device that enables music enthusiasts and small labels to create their own vinyl recordings.",
+
+                subtitle2: "Create your own records.",
+
+                paragraf2:
+                    "With exceptional precision and quality of craftsmanship, the SVR-82 provides a controlled and customized recording process. This machine is a true musical craft tool, allowing operators to cut grooves into blank vinyl discs to create their own unique pressings. The SVR-82 offers flexibility in choosing the type of vinyl, rotation speed, and cutting depth, making it suitable for a wide variety of musical styles. This vinyl recording machine is a treasure for audiophiles, vinyl collectors, and independent musicians who want to bring their musical creations to the classic analog format with a touch of authenticity and uniqueness.",
+
+                botao2: "Contact Us",
+
+                titleDiamond:
+                    "Precision Crafting and Sonic Excellence with Diamond Stylus Technology",
+
+                subtitleDiamond: "Precision",
+
+                paragrafDiamond:
+                    "Delve into a world of unparalleled precision and sonic purity with the Diamond Stylus for cutting and recording lathes. This meticulously crafted tool serves a dual purpose, delicately cutting through plastic surfaces and subsequently capturing music with minimal surface noise. Crafted by the skilled professionals at JC Audio, our trusted partner in delivering top-notch audio equipment, these diamond stylus needles epitomize the intersection of craftsmanship and innovation. Elevate your audio experience as you embrace the seamless fusion of cutting-edge technology and the artistry of sound.",
+
+                titleCrabide:
+                    "Tungsten Carbide Brilliance: Unleashing Precision in Embossing and Cutting",
+
+                subtitleCarbide: "Embossing and Cutting",
+
+                paragrafCarbide:
+                    "Unlock a realm of precision and durability with our Tungsten Carbide stylus, a true marvel in the world of embossing and precision cutting. Not only reserved for creating mesmerizing embossed patterns, but this tungsten carbide variant is also expertly honed to sharpen our stylus, ensuring impeccable precision when cutting through discs. This is particularly evident when paired with our soft blanks, allowing for a seamless convergence of strength and finesse. Experience the pinnacle of craftsmanship and innovation as our tungsten carbide stylus redefines the boundaries of embossing and cutting in the world of audio production.",
+
+                petg: "The Elegance of Vinyl: PETG Smoked Blank Discs for Diamond and Carbide Stylus Precision",
+
+                subtitlePetg: "PETG Smoked Blank",
+
+                paragrafPetg:
+                    "Step into a realm of audio finesse with our PETG Smoked Blank Vinyl Discs—crafted for excellence in both diamond and carbide stylus precision. Specially formulated for cutting with diamond needles and compatible with our carbide stylus as well, our PETG boasts exceptional quality and smoothness. These discs, created as a single piece, epitomize the seamless convergence of durability and sonic artistry. DJs, in particular, appreciate the significance of the smoked PETG disc, as it not only enhances the visual allure of vinyl but also ensures impeccable sound reproduction. The unique combination of high-quality material and one-piece construction makes these discs an essential choice for DJs seeking both style and precision in their vinyl performances. Immerse yourself in the rich, analog allure of vinyl records produced on these exceptional PETG blanks, and elevate your DJ experience to new heights.",
+
+                crystal:
+                    "Crystal Clarity and Sonic Smoothness: Blank Crystal Vinyl Discs for Unparalleled Elegance",
+
+                subtitleCrystal: " Crystal Blank",
+
+                paragrafCrystal:
+                    "Indulge in the epitome of elegance and smooth sonic performance with our Blank Crystal Vinyl Discs. Crafted to perfection, these discs are designed for precision with both diamond and carbide styluses. The exceptional clarity of our crystal blanks adds a touch of sophistication to your vinyl collection, ensuring a visual and auditory experience that transcends expectations. The material's inherent smoothness enhances the overall sound quality, making each note resonate with clarity and finesse. DJs, in particular, appreciate the unparalleled elegance and smoothness of our Crystal Blank Discs, as they contribute to seamless mixes and an immersive auditory journey. Elevate your vinyl experience with the luxurious combination of crystal clarity and sonic smoothness that defines our Blank Crystal Vinyl Discs.",
+
+                    diamontTest: "Diamond Stylus",
+                diamontTest2: "Diamond Stylus",
+                diamontTest3: "Diamond Stylus",
+                recordsTest: "Our Recordings Tests",
+                recordsTestsubt:
+                    "These are the test cuts we did to showcase the quality of our cuts.",
+                carbideTest: "Carbide Stylus",
+                carbideTest2: "Carbide Stylus",
+                carbideTest3: "Carbide Stylus",
+                
+            };
---
+++ languages/language-pt.js
@@ -0,0 +1,76 @@
+// language-pt.js
+            const translationsPt = {
+                home: "HOME",
+                lathe: "TORNOS",
+                styli: "AGULHAS",
+                blanks: "DISCOS",
+                records: "CORTES",
+                contact: "CONTATO",
+                contact2: "CONTATO",
+                latheContact:"Entre em Contato",
+                latheContact2:"Entre em Contato",
+                description:"A South.in Cuts é uma fornecedora de matéria-prima para tornos de gravação. Nós fornecemos máquinas de corte, agulhas e discos em branco. Além disso, oferecemos serviços de gravação em discos com a sua música personalizada. Entre em contato conosco para adquirir materiais e soluções personalizadas para suas necessidades de usinagem e gravação.",
+
+                contacttext:
+                    "Entre em contato conosco e ficaremos felizes em esclarecer todas as suas dúvidas sobre os nossos equipamentos. Nossos mixers rotativos e nossos tornos de gravação estão à disposição, prontos para atender às suas necessidades. Não hesite em nos contatar para obter informações detalhadas sobre como nossos produtos podem melhorar a qualidade e eficiência do seu trabalho. Estamos ansiosos para ajudá-lo a encontrar a solução perfeita para os seus projetos.",
+
+                SVR84: "A máquina gravadora de vinil SVR-84 - Record Lathe é uma evolução impressionante da tecnologia de corte de discos de vinil.",
+
+                subtitle: "Uma revolução em cortar discos",
+
+                paragraf:
+                    "Com recursos de última geração, essa máquina oferece aos profissionais da música e estúdios de gravação uma maneira eficiente e altamente precisa de criar vinis personalizados. O SVR-84 é equipado com recursos avançados, como controle de precisão CNC, que permite ajustar com extrema exatidão a profundidade e a largura dos sulcos, resultando em uma qualidade sonora excepcional. Além disso, seu sistema de monitoramento em tempo real e automação avançada garantem a consistência e a qualidade de cada gravação. Compatível com formatos de vinil padrão e personalizados, a SVR-84 atende às demandas dos músicos, gravadoras e audiófilos que desejam criar registros de vinil excepcionais e exclusivos em um ambiente de produção de alta tecnologia. Essa máquina gravadora de vinil representa a fusão perfeita entre a tradição do vinil e a moderna engenharia de precisão.",
+
+                botao: "Entre em Contato",
+
+                SVR82: "A máquina gravadora de vinil SVR-82 - Record Lathe é um dispositivo excepcional que permite aos entusiastas da música e aos pequenos selos criar suas próprias gravações em vinil.",
+
+                subtitle2: "Faça seus próprios discos",
+
+                paragraf2:
+                    "Com precisão e qualidade de fabricação excepcionais, a SVR-82 oferece um processo de gravação controlado e personalizado. Essa máquina é uma verdadeira ferramenta de artesanato musical, permitindo que os operadores cortem sulcos em discos de vinil virgens para criar suas próprias prensagens exclusivas. A SVR-82 oferece flexibilidade na escolha do tipo de vinil, velocidade de rotação e profundidade de corte, tornando-a adequada para uma ampla variedade de estilos musicais. Essa máquina gravadora de vinil é um tesouro para audiófilos, colecionadores de vinis e músicos independentes que desejam trazer suas criações musicais para o formato analógico clássico com um toque de autenticidade e singularidade.",
+
+                botao2: "Entre em Contato",
+
+                titleDiamond:
+                    "Precisão Artesanal e Excelência Sônica com a Tecnologia de Stylus de Diamante",
+
+                subtitleDiamond: "Precisão Artesanal ",
+
+                paragrafDiamond:
+                    "Adentre um mundo de precisão incomparável e pureza sonora com o Stylus de Diamante para corte e gravação em tornos. Esta ferramenta meticulosamente elaborada serve a um propósito duplo, cortando delicadamente superfícies plásticas e capturando música com ruído superficial mínimo. Criadas pelos habilidosos profissionais da JC Audio, nossa parceira confiável na entrega de equipamentos de áudio de alta qualidade, essas agulhas de stylus de diamante epitomizam a interseção entre a maestria artesanal e a inovação. Eleve sua experiência de áudio ao abraçar a fusão perfeita entre tecnologia de ponta e a arte do som.",
+
+                titleCrabide:
+                    "Brilho do Carbeto de Tungstênio: Liberação de Precisão em Embossing e Corte",
+
+                subtitleCarbide: " Precisão em Embossing e Corte",
+
+                paragrafCarbide:
+                    "Desbloqueie um reino de precisão e durabilidade com nosso stylus de Carbeto de Tungstênio, uma verdadeira maravilha no mundo do embossing e corte de precisão. Não reservado apenas para criar padrões em alto relevo fascinantes, essa variante de carbeto de tungstênio também é habilmente afiada para aprimorar nosso stylus, garantindo uma precisão impecável ao cortar discos. Isso é particularmente evidente quando emparelhado com nossos blanks suaves, permitindo uma convergência perfeita entre força e finesse. Experimente o ápice da maestria artesanal e inovação, pois nosso stylus de carbeto de tungstênio redefine os limites do embossing e corte no mundo da produção de áudio.",
+
+                petg: "A Elegância do Vinil: Discos PETG Fumê para Precisão com Agulhas de Diamante e Carbeto",
+
+                subtitlePetg: "Discos PETG Fumê",
+
+                paragrafPetg:
+                    "Adentre um mundo de refinamento sonoro com nossos Discos de Vinil PETG Fumê — elaborados para a excelência na precisão com agulhas de diamante e carbeto. Formulados especialmente para o corte com agulhas de diamante e compatíveis também com nossas agulhas de carbeto, nossos discos PETG destacam-se pela qualidade excepcional e suavidade. Criados como uma única peça, esses discos personificam a convergência perfeita entre durabilidade e arte sonora. DJs, em particular, reconhecem a importância do disco PETG fumê, pois ele não apenas aprimora o apelo visual do vinil, mas também garante uma reprodução sonora impecável. A combinação única de material de alta qualidade e construção única torna esses discos uma escolha essencial para DJs em busca de estilo e precisão em suas performances com vinil. Mergulhe na rica sedução analógica dos discos de vinil produzidos com esses excepcionais blanks PETG e eleve sua experiência como DJ a novas alturas.",
+
+                crystal:
+                    "Clareza Cristalina e Suavidade Sonora: Discos de Vinil Blank Crystal para Elegância Inigualável",
+
+                subtitleCrystal: "Petg Crystal",
+
+                paragrafCrystal:
+                    "Deleite-se na quintessência da elegância e desempenho sonoro suave com nossos Discos de Vinil Blank Crystal. Elaborados com perfeição, esses discos são projetados para precisão tanto com agulhas de diamante quanto de carbeto. A excepcional clareza dos nossos blanks de cristal adiciona um toque de sofisticação à sua coleção de vinil, garantindo uma experiência visual e auditiva que transcende expectativas. A suavidade inerente do material aprimora a qualidade sonora geral, fazendo com que cada nota ressoe com clareza e finesse. DJs, em particular, apreciam a elegância e suavidade incomparáveis dos nossos Discos Blank Crystal, pois contribuem para mixagens perfeitas e uma jornada auditiva imersiva. Eleve sua experiência com vinil com a luxuosa combinação de clareza cristalina e suavidade sonora que define nossos Discos de Vinil Blank Crystal.",
+
+
+                    diamontTest: "Agulha de Diamante",
+                diamontTest2: "Agulha de Diamante",
+                diamontTest3: "Agulha de Diamante",
+                recordsTest: "Teste de Gravação",
+                recordsTestsubt:
+                    "Estes são os cortes de teste que fizemos para demonstrar a qualidade dos nossos cortes.",
+                carbideTest: "Agulha de Carbide",
+                carbideTest2: "Agulha de Carbide",
+                carbideTest3: "Agulha de Carbide",
+            };
